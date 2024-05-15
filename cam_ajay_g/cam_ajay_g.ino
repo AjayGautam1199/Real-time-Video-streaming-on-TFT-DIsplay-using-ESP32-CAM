b0bd525d0b2898678caa3a2316f0d7bacfba7117
AjayGautam1199 <107488026+AjayGautam1199@users.noreply.github.com>
2024-05-15 13:38:02 +0530

    use

---
+++ cam_ajay_g/cam_ajay_g.ino
@@ -0,0 +1,196 @@
+#include <esp32cam.h>
+#include <lvgl.h>
+#include <LovyanGFX.hpp>
+#include "esp_camera.h"
+#include <SPI.h>
+#include <TJpg_Decoder.h>
+// #include "sd_store_AjayG.h"
+#define CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+class LGFX : public lgfx::LGFX_Device {
+  lgfx::Panel_ILI9341 _panel_instance;
+  lgfx::Bus_SPI _bus_instance;
+  lgfx::Light_PWM _light_instance;
+
+public:
+  LGFX(void) {
+    {
+      auto cfg = _bus_instance.config();
+      cfg.spi_host = VSPI_HOST;
+      cfg.spi_mode = 0;
+      cfg.freq_write = 40000000;
+      cfg.freq_read = 16000000;
+      cfg.spi_3wire = false;
+      cfg.use_lock = true;
+      cfg.dma_channel = 1;
+      cfg.pin_sclk = 13;
+      cfg.pin_mosi = 15;
+      cfg.pin_miso = 14;
+      cfg.pin_dc = 2;
+      _bus_instance.config(cfg);
+      _panel_instance.setBus(&_bus_instance);
+    }
+
+    {
+      auto cfg = _panel_instance.config();
+      cfg.pin_cs = 12;
+      cfg.pin_rst = 4;
+      cfg.panel_width = 240;
+      cfg.panel_height = 320;
+      cfg.offset_x = 0;
+      cfg.offset_y = 0;
+      cfg.offset_rotation = 0;
+      cfg.dummy_read_pixel = 8;
+      cfg.dummy_read_bits = 1;
+      cfg.readable = true;
+      cfg.invert = false;
+      cfg.rgb_order = false;
+      cfg.dlen_16bit = false;
+      cfg.bus_shared = true;
+      _panel_instance.config(cfg);
+    }
+
+    {
+      auto cfg = _light_instance.config();
+      cfg.invert = false;
+      cfg.freq = 44100;
+      cfg.pwm_channel = 7;
+      _light_instance.config(cfg);
+      _panel_instance.setLight(&_light_instance);
+    }
+
+    setPanel(&_panel_instance);
+  }
+};
+
+static LGFX tft;
+
+static const uint16_t screenWidth = 240;
+static const uint16_t screenHeight = 320;
+
+static lv_disp_draw_buf_t draw_buf;
+static lv_color_t buf[screenWidth * screenHeight / 10];
+
+static lv_disp_drv_t disp_drv;
+
+void lvgl_init() {
+  lv_init();
+  lv_disp_draw_buf_init(&draw_buf, buf, NULL, screenWidth * screenHeight / 10);
+  lv_disp_drv_init(&disp_drv);
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
+}
+
+
+
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
+  uint32_t w = (area->x2 - area->x1 + 1);
+  uint32_t h = (area->y2 - area->y1 + 1);
+
+  tft.startWrite();
+  tft.setAddrWindow(area->x1, area->y1, w, h);
+  tft.writePixels((lgfx::rgb565_t *)&color_p->full, w * h);
+  tft.endWrite();
+
+  lv_disp_flush_ready(disp);
+}
+void cameraInit(){
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 10000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_QVGA; //320x240
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+void showingImage(){
+  camera_fb_t* fb = esp_camera_fb_get();
+  if(!fb || fb->format != PIXFORMAT_JPEG){
+    Serial.println("Camera Capture Failed!");
+  }else{
+    Serial.println("Camera Image to Display Here!");
+    TJpgDec.drawJpg(0, 0, (const uint8_t*)fb->buf, fb->len);
+  }
+  esp_camera_fb_return(fb);
+}
+
+bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap){
+  if( y>= tft.height()) return 0;
+  tft.pushImage(x, y, w, h, bitmap);
+  return 1;
+}
+
+
+
+void setup() {
+  Serial.begin(115200);
+  lvgl_init();
+  displayInit();
+  cameraInit();
+  // pinMode(BUTTON_PIN, INPUT_PULLUP);
+  // initMicroSDCard();
+  Serial.println("Setup done!");
+}
+
+void loop() {
+  showingImage();
+
+  //   // Take and Save Photo
+  // if(digitalRead(BUTTON_PIN==LOW)){
+  //   if(digitalRead(BUTTON_PIN==LOW)){
+  //     takeSavePhoto();
+  //   }
+  // }
+
+
+}
+
+void displayInit(){
+  tft.begin();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_WHITE);
+
+  TJpgDec.setJpgScale(1);
+  TJpgDec.setSwapBytes(true);
+  TJpgDec.setCallback(tft_output);
+}
---
+++ cam_ajay_g/sd_store_AjayG.h
@@ -0,0 +1,39 @@
+#include "FS.h"                // SD Card ESP32
+#include "SD_MMC.h"            // SD Card ESP32
+
+#define BUTTON_PIN 16
+
+void initMicroSDCard() {
+SD_MMC.begin();
+}
+
+String getPictureFilename(){
+  String filename = "/picture_" + String(random(1,3600)) +".jpg";
+  return filename; 
+}
+void takeSavePhoto(){
+    
+  camera_fb_t * fb = esp_camera_fb_get();
+ 
+  
+  if(!fb) {
+    ESP.restart();
+  }
+
+  String path = getPictureFilename();
+  Serial.printf("Picture file name: %s\n", path.c_str());
+  
+  // Save picture to microSD card
+  fs::FS &fs = SD_MMC; 
+  File file = fs.open(path.c_str(),FILE_WRITE);
+  if(!file){
+    Serial.printf("Failed to open file in writing mode");
+  } 
+  else {
+    file.write(fb->buf, fb->len); // payload (image), payload length
+    Serial.printf("Saved: %s\n", path.c_str());
+  }
+  file.close();
+  esp_camera_fb_return(fb); 
+}
+
